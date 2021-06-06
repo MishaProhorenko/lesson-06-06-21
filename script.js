5bf5ae7d6443f417fc251746d9183ee864a2ba1e
Misha Prohorenko <mishaprohorenko96@gmail.com>
2021-06-06 18:51:49 +0300

    qwe

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
 
+let count = 0;
+
+block.addEventListener('mouseover', (e) => {	
+	e.target.classList.add('middle');
+	e.target.innerText = `Hoverd by mounse ${count++} times`
+
+})
+block.addEventListener('mouseout', (e) => {
+	e.target.classList.remove('middle');
+	e.target.classList.add('block');		
+})
+
+
