790b5aac97f70a453e86757bb16506855ead3009
Misha Prohorenko <mishaprohorenko96@gmail.com>
2021-06-06 20:53:44 +0300

    www

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 'use strict';
 
 
-let count = 0;
+//focus
 
-block.addEventListener('mouseover', (e) => {	
-	e.target.classList.add('middle');
-	e.target.innerText = `Hoverd by mounse ${count++} times`
 
-})
-block.addEventListener('mouseout', (e) => {
-	e.target.classList.remove('middle');
-	e.target.classList.add('block');		
-})
+//onfocus
+
+
+let user = {
+	name: 'Rajif',
+	surname: 'Das',
+	personalId: 'x23x123x22',
+}
+
+
+
+// ourInput.addEventListener('focus', (e) => {
+// 	// e.target.value = `${user.name} ${user.surname}`;
+
+// });
+// ourInput.addEventListener('blur', (e) => {
+// 	// e.target.value = `${user.personalId} (${user.name} ${user.surname})`;
+// 	console.log('blur');
+// });
+
+ourInput.addEventListener('focus', (e) => {
+	e.target.style.color = '';
+
+});
+ourInput.addEventListener('focus', (event) => {
+	event.target.style.color = '';
+});
+
+ourInput.addEventListener('change', (event) => {
+	if (event.target.value.length < 8) {
+		event.target.style.color = 'red';
+		errorSection.innerText = `${event.target.value} is invaid`;
+	} else {
+		const li = document.createElement('li');
+		li.innerText = event.target.value;
+		list.append(li);
+		errorSection.innerText = '';
+		event.target.value = '';
+	}
+});
+
+
+
+
+
+
+
+
 
 
